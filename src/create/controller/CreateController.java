9e0262e6c1ee7b6804202be661436f3a2c97407c
Dngn20 <dngn20@gmail.com>
2017-01-09 13:27:12 -0700

    Made Create Porject and made Framework

---
+++ src/create/controller/CreateController.java
@@ -0,0 +1,9 @@
+package create.controller;
+
+public class CreateController 
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/create/controller/CreateRunner.java
@@ -0,0 +1,10 @@
+package create.controller;
+
+public class CreateRunner 
+{
+	public static void main (String [] args)
+	{
+		CreateController CreateappController = new CreateController();
+		CreateappController.start();
+	}
+}
