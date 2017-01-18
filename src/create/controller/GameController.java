75c0201240821e1dc8271866e7d7062e91901b41
Dngn20 <dngn20@gmail.com>
2017-01-18 13:55:38 -0700

    Layout god and temp making

--- src/create/controller/GameController.java
+++ src/create/controller/GameController.java
@@ -1,9 +1,24 @@
 package create.controller;
 
+import create.view.CreateFrame;
+
+
 public class GameController 
 {
+	private CreateFrame baseFrame;
+
+	public GameController()
+	{
+		baseFrame = new CreateFrame(this);
+	}
+	
 	public void start()
 	{
 		
+	}
+	
+	public CreateFrame getBaseFrame()
+	{
+		return baseFrame;
 	}
 }
--- src/create/view/CreatePanel.java
+++ src/create/view/CreatePanel.java
@@ -13,7 +13,7 @@ public class CreatePanel extends JPanel
 	private JButton OOEButton;
 	private JLabel gameLabel;
 	private SpringLayout baseLayout;
-
+	private JLabel titleLabel;
 	
 	
 	
@@ -27,10 +27,12 @@ public class CreatePanel extends JPanel
 		this.RPSButton = new JButton("Rock, Paper, Scissors Game");
 		this.OOEButton = new JButton("Odds or Even Game");
 		this.gameLabel = new JLabel("You can play three games with the application.");
+		this.titleLabel = new JLabel("SIMPLE GAMES");
+		
 		
 	
 		setupPanel();
-		setupPanel();
+		setupLayout();
 		
 	}
 	
@@ -44,6 +46,9 @@ public class CreatePanel extends JPanel
 		gameLabel.setVerticalTextPosition(JLabel.TOP);
 		gameLabel.setHorizontalTextPosition(JLabel.CENTER);
 		
+		titleLabel.setFont(new Font("Malayalam MN", Font.PLAIN, 50));
+		
+		this.add(titleLabel);
 		this.add(TTTButton);
 		this.add(RPSButton);
 		this.add(OOEButton);
@@ -53,14 +58,26 @@ public class CreatePanel extends JPanel
 
 	public void setupLayout()
 	{
-		baseLayout.putConstraint(SpringLayout.WEST, TTTButton, 159, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, TTTButton, -227, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, TTTButton, 265, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, TTTButton, 53, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, RPSButton, 0, SpringLayout.NORTH, TTTButton);
-		baseLayout.putConstraint(SpringLayout.WEST, RPSButton, 28, SpringLayout.EAST, TTTButton);
+		baseLayout.putConstraint(SpringLayout.WEST, RPSButton, 90, SpringLayout.EAST, TTTButton);
 		baseLayout.putConstraint(SpringLayout.NORTH, OOEButton, 0, SpringLayout.NORTH, TTTButton);
-		baseLayout.putConstraint(SpringLayout.WEST, OOEButton, 57, SpringLayout.EAST, RPSButton);
-		baseLayout.putConstraint(SpringLayout.WEST, gameLabel, 0, SpringLayout.WEST, TTTButton);
-		baseLayout.putConstraint(SpringLayout.SOUTH, gameLabel, -23, SpringLayout.NORTH, TTTButton);
+		baseLayout.putConstraint(SpringLayout.WEST, OOEButton, 99, SpringLayout.EAST, RPSButton);
+		baseLayout.putConstraint(SpringLayout.WEST, gameLabel, 265, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, gameLabel, -25, SpringLayout.NORTH, RPSButton);
+		baseLayout.putConstraint(SpringLayout.WEST, titleLabel, 222, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, titleLabel, -34, SpringLayout.NORTH, gameLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, TTTButton, 32, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, TTTButton, -269, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, RPSButton, 0, SpringLayout.NORTH, TTTButton);
+		baseLayout.putConstraint(SpringLayout.WEST, RPSButton, 114, SpringLayout.EAST, TTTButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, OOEButton, 0, SpringLayout.NORTH, TTTButton);
+		baseLayout.putConstraint(SpringLayout.EAST, OOEButton, -85, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, gameLabel, 268, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, titleLabel, -346, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, gameLabel, 5, SpringLayout.SOUTH, titleLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, titleLabel, 228, SpringLayout.WEST, this);
 	}
 
 
